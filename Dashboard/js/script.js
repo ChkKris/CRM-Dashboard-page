bea7ea098a3aac9ed06cc30a823944b545b85df5
ChkKris <kristina141098@gmail.com>
2024-01-04 19:42:53 +0200

    update

---
+++ Dashboard/js/script.js
@@ -0,0 +1,10 @@
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.sidebar__menu');
+
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
