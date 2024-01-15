440ae80cfbf81cffc46ab53e9b3cb77660bc14c4
ChkKris <kristina141098@gmail.com>
2024-01-15 20:43:33 +0200

    update

--- Dashboard/js/script.js
+++ Dashboard/js/script.js
@@ -1,5 +1,5 @@
 const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.sidebar__menu');
+const menuBody = document.querySelector('.sidebar__container');
 
 if (iconMenu) {
 	iconMenu.addEventListener('click', function (e) {
